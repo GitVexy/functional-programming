a65d085f0e7576eafc14885a589f5f0ea808899c
GitVexy <erik.appsec@gmail.com>
2025-01-24 01:22:56 +0100

    Optional challenge 1 in chapter 4 completed.

--- ch04/lc01.py
+++ ch04/lc01.py
@@ -1,2 +1,32 @@
 def count_nested_levels(nested_documents, target_document_id, level=1):
-    pass
+    print(f"Searching in {nested_documents}")
+    if not nested_documents:
+        return -1
+    
+    for key in nested_documents:
+        
+        if key == target_document_id:
+            print(f"\nTarget found in branch: {nested_documents}\nLevel: {level}\nReturning\n")
+            return level
+        
+        elif nested_documents[key] != {}:
+            nested_level = count_nested_levels(
+                            nested_documents[key],
+                            target_document_id, level + 1)
+            
+            if nested_level != -1:
+                return nested_level
+
+    return -1
+
+## Testing:
+
+test_tree = {1: {2: {3: {}, 4: {5: {}}}, 6: {}, 7: {8: {9: {10: {}}}}}}
+target_id = 9
+
+print("\n")
+print(f"Running count_nested_levels with:",
+      f"\nnested_documents: {test_tree}",
+      f"\ntarget_id: {target_id}\n")
+print(f"Result: {count_nested_levels(test_tree, target_id)}")
+print("\n")
