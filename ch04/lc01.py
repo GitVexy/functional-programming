c0390273340274db64b536bfb7af4e6487c3cfaf
GitVexy <erik.appsec@gmail.com>
2025-01-24 01:41:28 +0100

    Minor changes to ch04/lc01.py

--- ch04/lc01.py
+++ ch04/lc01.py
@@ -1,22 +1,24 @@
 def count_nested_levels(nested_documents, target_document_id, level=1):
-    print(f"Searching in {nested_documents}")
     if not nested_documents:
         return -1
     
     for key in nested_documents:
         
         if key == target_document_id:
-            print(f"\nTarget found in branch: {nested_documents}\nLevel: {level}\nReturning\n")
+            print(f"\nTarget found in branch: {nested_documents}\nLevel: {level}\n")
+            print(f"Returned {level} from key {key}")
             return level
         
         elif nested_documents[key] != {}:
+            print(f"Going deeper into key {key} with value {nested_documents[key]}")
             nested_level = count_nested_levels(
                             nested_documents[key],
                             target_document_id, level + 1)
+            print(f"Returned {nested_level} from key {key}")
             
             if nested_level != -1:
                 return nested_level
-
+    
     return -1
 
 ## Testing:
@@ -28,5 +30,5 @@ print("\n")
 print(f"Running count_nested_levels with:",
       f"\nnested_documents: {test_tree}",
       f"\ntarget_id: {target_id}\n")
-print(f"Result: {count_nested_levels(test_tree, target_id)}")
+print(f"\nResult: {count_nested_levels(test_tree, target_id)}")
 print("\n")
